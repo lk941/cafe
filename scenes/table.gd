4327d81f2fdc372fa7eee9c83c40049c681c9b80
lk941 <leekee941@gmail.com>
2026-03-31 15:10:55 +0800

    splash screen

--- scenes/table.gd
+++ scenes/table.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
-@onready var card = $background
+@onready var card = $foryoucard
+@onready var menu = $menucontents
 
 
 func _on_card_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	card.visible = true
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		card.visible = true
 
 
 func _on_background_gui_input(event: InputEvent) -> void:
 	card.visible = false
+
+
+func _on_menu_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		menu.visible = true
+
+
+func _on_menucontents_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		menu.visible = false
